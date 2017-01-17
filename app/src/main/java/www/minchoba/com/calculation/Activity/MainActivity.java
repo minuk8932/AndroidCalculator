40b29825ad16407e149e5b35845ad8a0f14479c0
minwook shin <minuk8932@naver.com>
2017-01-17 16:57:51 +0900

    AndCalculator

---
+++ app/src/main/java/www/minchoba/com/calculation/Activity/MainActivity.java
@@ -0,0 +1,107 @@
+package www.minchoba.com.calculation.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import www.minchoba.com.calculation.Listener.OnBtnClickListener;
+
+import www.minchoba.com.calculation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button acBtn;
+    private Button divBtn;
+    private Button sumBtn;
+    private Button subBtn;
+    private Button timesBtn;
+    private Button equalBtn;
+    private Button zeroBtn;
+    private Button oneBtn;
+    private Button twoBtn;
+    private Button threeBtn;
+    private Button fourBtn;
+    private Button fiveBtn;
+    private Button sixBtn;
+    private Button sevenBtn;
+    private Button eightBtn;
+    private Button nineBtn;
+    private TextView resTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // android's main
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // 이를 지정안하면 app이 죽는다.
+
+        resTextView = (TextView) findViewById(R.id.main_result_textview);
+
+        acBtn = (Button) findViewById(R.id.main_btn_ac); // button을 객체생성해서 xml의 button을 가져옴.
+        acBtn.setTag(OnBtnClickListener.BTN_AC);
+        acBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView)); // Listener Pack 1.에서 만든 class 객체 생성.
+                                                            // outline style로 main을 간결하게, 그리고 유지보수하기 편하게 생성
+
+        divBtn = (Button) findViewById(R.id.main_btn_div);
+        divBtn.setTag(OnBtnClickListener.BTN_DIV);
+        divBtn.setOnClickListener(new OnBtnClickListener(MainActivity.this,resTextView)); //getBaseContext() = MainActivity.this
+
+        sumBtn = (Button) findViewById(R.id.main_btn_sum);
+        sumBtn.setTag(OnBtnClickListener.BTN_SUM);
+        sumBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView)); // main에서 인자를 1개더 받는모습
+
+        subBtn = (Button) findViewById(R.id.main_btn_sub);
+        subBtn.setTag(OnBtnClickListener.BTN_SUB);
+        subBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        timesBtn = (Button) findViewById(R.id.main_btn_times);
+        timesBtn.setTag(OnBtnClickListener.BTN_TIMES);
+        timesBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        equalBtn = (Button) findViewById(R.id.main_btn_equal);
+        equalBtn.setTag(OnBtnClickListener.BTN_EQUAL); // OnBtnClickListener에서 pulbic 선언된 상수를 가져와서 가독성을 풀어준다.
+        equalBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        zeroBtn = (Button) findViewById(R.id.main_btn_0);
+        zeroBtn.setTag(0);
+        zeroBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        oneBtn = (Button) findViewById(R.id.main_btn_1);
+        oneBtn.setTag(1);
+        oneBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        twoBtn = (Button) findViewById(R.id.main_btn_2);
+        twoBtn.setTag(2);
+        twoBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        threeBtn = (Button) findViewById(R.id.main_btn_3);
+        threeBtn.setTag(3);
+        threeBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        fourBtn = (Button) findViewById(R.id.main_btn_4);
+        fourBtn.setTag(4);
+        fourBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        fiveBtn = (Button) findViewById(R.id.main_btn_5);
+        fiveBtn.setTag(5);
+        fiveBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        sixBtn = (Button) findViewById(R.id.main_btn_6);
+        sixBtn.setTag(6);
+        sixBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        sevenBtn = (Button) findViewById(R.id.main_btn_7);
+        sevenBtn.setTag(7);
+        sevenBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        eightBtn = (Button) findViewById(R.id.main_btn_8);
+        eightBtn.setTag(8);
+        eightBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+
+        nineBtn = (Button) findViewById(R.id.main_btn_9);
+        nineBtn.setTag(9);
+        nineBtn.setOnClickListener(new OnBtnClickListener(getBaseContext(),resTextView));
+    }
+
+}
---
+++ app/src/main/java/www/minchoba/com/calculation/Listener/OnBtnClickListener.java
@@ -0,0 +1,101 @@
+package www.minchoba.com.calculation.Listener;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.eclipsesource.v8.V8;
+
+
+/**
+ * Created by minchoba on 2017. 1. 17..
+ */
+public class OnBtnClickListener implements View.OnClickListener { // 1. class를 이용해 간결히 만들자.
+    public static final int BTN_AC = -1;
+    public static final int BTN_DIV = -2;
+    public static final int BTN_SUM = -3;
+    public static final int BTN_SUB = -4;
+    public static final int BTN_TIMES = -5;
+    public static final int BTN_EQUAL = -6;
+
+    private Context context;
+    private TextView resTextView;
+    private static StringBuilder sb;
+
+    public OnBtnClickListener(Context context, TextView resTextView) { // findViewById는 activity에서만 사용 가능 하므로
+                                                                //TextView 인자를 생성해서 main으로 넘겨준다.
+
+        this.context = context; // 현 화면의 문맥, 상태를 가져 올 것이야
+        this.resTextView = resTextView;
+        this.sb = new StringBuilder();
+        sb.append(resTextView.getText());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button btn = (Button) view;
+
+        switch ((int) btn.getTag()) {
+            case BTN_AC:
+//                resTextView.setText("0");
+
+                sb = new StringBuilder();
+                sb.append("0");
+                break;
+            case BTN_DIV:
+                sb.append("/");
+                break;
+            case BTN_SUM:
+                sb.append("+");
+                break;
+            case BTN_SUB:
+                sb.append("-");
+                break;
+            case BTN_TIMES:
+                sb.append("*");
+                break;
+            case BTN_EQUAL:
+                Toast.makeText(context,"equal",Toast.LENGTH_SHORT).show();
+
+                V8 runtime = V8.createV8Runtime(); // gradle에서 complie한 V8 오픈 소스를 가져와 쓴다.
+                int result = runtime.executeIntegerScript("eval("+ sb.toString() +")");
+                                                //eval () : 괄호 내 수식을 계산해 뽑아주는 함수.
+                runtime.release();
+
+                sb = new StringBuilder();
+                sb.append(String.valueOf(result));
+
+                break;
+
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                if(resTextView.getText().equals("0")){ // 만약 resTextView의 숫자가 0이었다면,
+                    sb = new StringBuilder();
+                    sb.append(String.valueOf(btn.getTag()));
+//                    resTextView.setText((String.valueOf(btn.getTag()))); // resTextView의 text를
+//                                                                // 눌린 btn의 태그값을 String형으로 가져와서 바꾸어 setting.
+                }
+                else{
+                    sb.append(String.valueOf(btn.getTag()));
+//                    resTextView.setText(resTextView.getText() + String.valueOf(btn.getTag()));
+//                                                                // 0이 아니었다면, resTextView의 text를 resTextView의 text에 이어
+//                                                                // btn의 태그값을 String형으로 가져와 setting.
+                }
+
+                break;
+
+        }
+        resTextView.setText(sb.toString());
+    }
+}
